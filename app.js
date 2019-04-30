325b16d05213f121dd840648c775b748f2d758d8
SkyZyy <sergito1997.sd@gmail.com>
2019-04-30 10:16:57 +0100

    Trying to deploy attemp 2

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const routes = require('./routes/index');
 const bodyParser = require('body-parser');
 
 const app = express();
 const cons = require('consolidate');
+const server = http.createServer(app);
 
 app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
@@ -15,4 +17,4 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
 
-module.exports = app;
+module.exports = server;
